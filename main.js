6eec421b423084878b3e22fecc884a1e041973f9
Travis Anderson <thetanderson@gmail.com>
2018-05-17 16:13:07 -0400

    updated win so it goes to another maze

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const map = [
+let map = [
     "  WWWWW ",
     "WWW   W ",
     "WOSB  W ",
@@ -159,7 +159,23 @@ function win (){
 
     if(boxCount.length === boxOnSpotCount.length){
      alert("You win")
-     
+     main.innerHTML=""
+     map = [
+        "    WWWWW          ",
+        "    W   W          ",
+        "    WB  W          ",
+        "  WWW  BWW         ",
+        "  W  B B W         ",
+        "WWW W WW W   WWWWWW",
+        "W   W WW WWWWW  OOW",
+        "W B  B          OOW",
+        "WWWWW WWW WSWW  OOW",
+        "    W     WWWWWWWWW",
+        "    WWWWWWW        "
+     ]
+        boardHeight = map.length
+        boardWidth = map[0].length
+     createMaze()
  }
 }
 
@@ -169,5 +185,5 @@ document.addEventListener('keyup',win)
 
 createMaze()
 document.addEventListener('keydown', checkMove)
-document.addEventListener('click', )
+
 //document.addEventListener('keyup', checkWin)
