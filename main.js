031781c75f5eb0f9fafe59ee542abe352653b80d
Travis Anderson <thetanderson@gmail.com>
2018-05-17 14:36:06 -0400

    all basic functionality and win condition working, would like to add some pizzaz

--- main.js
+++ main.js
@@ -1,19 +1,33 @@
-const map = [
-    "  WWWWW ",
-    "WWW   W ",
-    "WOSB  W ",
-    "WWW BOW ",
-    "WOWWB W ",
-    "W W O WW",
-    "WB XBBOW",
-    "W   O  W",
-    "WWWWWWWW"
-  ];
+// const map = [
+//     "  WWWWW ",
+//     "WWW   W ",
+//     "WOSB  W ",
+//     "WWW BOW ",
+//     "WOWWB W ",
+//     "W W O WW",
+//     "WB XBBOW",
+//     "W   O  W",
+//     "WWWWWWWW"
+//   ];
+
+  const map = [
+    "    WWWWW          ",
+    "    W   W          ",
+    "    WB  W          ",
+    "  WWW  BWW         ",
+    "  W  B B W         ",
+    "WWW W WW W   WWWWWW",
+    "W   W WW WWWWW  OOW",
+    "W B  B          OOW",
+    "WWWWW WWW WSWW  OOW",
+    "    W     WWWWWWWWW",
+    "    WWWWWWW        "
+ ]
+
 const main = document.getElementById("main");
 let currentColumn = 1;
 let cellNumber = "";
 let rowNumber = "";
-
 //gamePiece is the active character the player directly controls
 let gamePiece = document.createElement("div");
 let pacmanDirection = "rotate(0)"
@@ -46,29 +60,31 @@ function createCell(row, displayRow) {
     }
 }
 
+//assigns class to cells based on if wall
 function assignCellType(cell, displayCell) {
     switch (cell) {
 
-        case cell = " ":
-            displayCell.classList.add("movable");
-            break;
         case cell = "W":
             displayCell.classList.add("wall");
             break;
 
         case cell = "S":
             displayCell.id = "startPosition";
-            displayCell.classList.add("movable");
             displayCell.appendChild(gamePiece);
             break;
 
             case cell = "O":
             displayCell.classList.add("O");
-            displayCell.classList.add("movable");
             break;
 
             case cell = "B":
             displayCell.classList.add("B");
+            // displayCell.appendChild(storageLocation)
+            break;
+
+            case cell = "X":
+            displayCell.classList.add("B");
+            displayCell.classList.add("O");
             // displayCell.appendChild(storageLocation)
             break;
     }
@@ -82,22 +98,23 @@ function moveCell(yOffset, xOffset,yTwoCellOffset,xTwoCellOffset) {
     const xTwoMoves = parseInt(gamePiece.parentElement.dataset.cellX) + xTwoCellOffset;
     const twoCellAway = document.querySelector('[data-cell-y="' + yTwoMoves + '"][data-cell-x="' + xTwoMoves + '"]');
     const nextCell = document.querySelector('[data-cell-y="' + yMove + '"][data-cell-x="' + xMove + '"]');
+    
 
+    //first if is for movement of game token if (cell not wall & notblock, move) OR (if next cell has block, and two cells away not block or wall, move)
     if ((!nextCell.classList.contains("wall") && !nextCell.classList.contains("B")) 
-        ||
-        ((nextCell.classList.contains("B") && !twoCellAway.classList.contains("B")))
+        ||(
+        (nextCell.classList.contains("B") && !twoCellAway.classList.contains("B")))
         && 
-        (nextCell.classList.contains("B") && !twoCellAway.classList.contains("wall"))){  
-         nextCell.appendChild(gamePiece);
-         console.log("hey")    
+        (nextCell.classList.contains("B") && !twoCellAway.classList.contains("wall"))
+        ){  
+        nextCell.appendChild(gamePiece);    
     }
-
+    // this if is for moving blocks, if nextCell is block, twoCells away not a wall or block, move classList to two cells away from token
     if(!twoCellAway.classList.contains("B") && nextCell.classList.contains("B") && !twoCellAway.classList.contains("wall")){
         nextCell.classList.remove("B");
         twoCellAway.classList.add("B")
     }
 }
- 
 //function checkmove(event) was originally used below, why do I need event???
 function checkMove() {
     event.preventDefault();
@@ -123,76 +140,27 @@ function checkMove() {
             moveCell(0, -1, 0, -2);
             pacmanDirection = "rotate(180deg)";
             break;
-
     }
-    document.getElementById("gamePiece").style.top = boxTop + "px";
-    document.getElementById("gamePiece").style.left = boxLeft + "px";
     document.getElementById("gamePiece").style.transform = pacmanDirection;
 }
-//original below, compare to above
-    // if (keyName === "ArrowDown") {
-    //     //move down
-    //     // console.log("cellY:", gamePiece.parentElement.dataset.cellY)
-    //     // console.log("cellX:", gamePiece.parentElement.dataset.cellX)
-    //     // let moveDown = parseInt(gamePiece.parentElement.dataset.cellY) + 1;
-    //     // let currentX = gamePiece.parentElement.dataset.cellX;
-    //     // //checks if classname is only cell, in this case this works, also could do a 'does not contain' className.contain = 
-    //     // if (document.querySelector('[data-cell-y="' + moveDown + '"][data-cell-x="' + currentX + '"]').className === "cell") {
-    //     //     document.querySelector('[data-cell-y="' + moveDown + '"][data-cell-x="' + currentX + '"]').appendChild(gamePiece);
-    //     // }
-    //     moveCell(+1, 0);
-    //     pacmanDirection = "rotate(90deg)";
-    // }
 
-    // //moves box up
-    // if (keyName === "ArrowUp") {
-    //     // console.log("cellY:", gamePiece.parentElement.dataset.cellY)
-    //     // console.log("cellX:", gamePiece.parentElement.dataset.cellX)
-    //     // let moveUp = parseInt(gamePiece.parentElement.dataset.cellY) - 1;
-    //     // let currentX = gamePiece.parentElement.dataset.cellX;
-    //     // if (document.querySelector('[data-cell-y="' + moveUp + '"][data-cell-x="' + currentX + '"]').className === "cell") {
-    //     //     document.querySelector('[data-cell-y="' + moveUp + '"][data-cell-x="' + currentX + '"]').appendChild(gamePiece);
-    //     // }
-    //     moveCell(-1, 0);
-    //     pacmanDirection = "rotate(-90deg)";
-    // }
-
-    // //moves box right
-    // if (keyName === "ArrowRight") {
-    //     //this is how to get parent element id
-    //     // console.log("cellY:", gamePiece.parentElement.dataset.cellY);
-    //     //console.log("cellX:", gamePiece.parentElement.dataset.cellX);
-    //     // let moveRight = parseInt(gamePiece.parentElement.dataset.cellX) + 1;
-    //     // let currentY = gamePiece.parentElement.dataset.cellY;
-    //     // if (document.querySelector('[data-cell-y="' + currentY + '"][data-cell-x="' + moveRight + '"]').className === ("cell")) {
-    //     //     //  console.log(document.querySelector('[data-cell-y="9"][data-cell-x="' + moveRight + '"]'));
-    //     //     document.querySelector('[data-cell-y="' + currentY + '"][data-cell-x="' + moveRight + '"]').appendChild(gamePiece);
-    //     // }
-    //     moveCell(0, +1)
-    //     pacmanDirection = "rotate(0)";
-    // }
-
-    // if (keyName === "ArrowLeft") {
-    //     // console.log("cellY:", gamePiece.parentElement.dataset.cellY);
-    //     // console.log("cellX:", gamePiece.parentElement.dataset.cellX);
-    //     // let moveLeft = parseInt(gamePiece.parentElement.dataset.cellX) - 1;
-    //     // let currentY = gamePiece.parentElement.dataset.cellY;
-    //     // if (document.querySelector('[data-cell-y="' + currentY + '"][data-cell-x="' + moveLeft + '"]').className === "cell") {
-    //     //     document.querySelector('[data-cell-y="' + currentY + '"][data-cell-x="' + moveLeft + '"]').appendChild(gamePiece);
-    //     // }
-    //     moveCell(0, -1);
-    //     pacmanDirection = "rotate(180deg)";
-    // }
 
     
 
 
+function win (){ 
+    const boxOnSpotCount = document.getElementsByClassName("O B");
+    const boxCount = document.getElementsByClassName("B");  
 
-// function checkWin(event) {
-//     if (document.getElementById("endPosition").childElementCount === 1) {
-//         alert("You win")
-//     }
-// }
+    if(boxCount.length === boxOnSpotCount.length){
+     alert("You win")
+     console.log("you win")
+ }
+}
+
+
+
+document.addEventListener('keyup',win)
 
 createMaze()
 document.addEventListener('keydown', checkMove)
